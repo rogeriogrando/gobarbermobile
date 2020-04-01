0b5efcceea04df40dbd441e432565dfb1e201fc0
Rogério Grando <rgrando.unique@gmail.com>
2020-03-31 23:47:57 -0300

    Fim

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 }
+})``;
